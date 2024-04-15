6d70416de5f85217c5ee7b836c85e47eab48f4d5
LasiSipsara <sipsaralasini@gmail.com>
2024-04-15 22:28:25 +0530

    uploaded full project

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.mycalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mycalculator.ui.theme.MediumGray
+import com.example.mycalculator.ui.theme.MyCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyCalculatorTheme {
+              val viewModel=viewModel<CalculatorViewModel> ()
+                val state= viewModel.state
+                val buttonSpacing= 8.dp
+
+                Calculator(
+                    state =state ,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MediumGray)
+                        .padding(16.dp)
+
+                )
+
+            }
+        }
+    }
+}
+
